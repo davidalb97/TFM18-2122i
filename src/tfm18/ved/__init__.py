0104a5acd820b02bcc6ef2b640b5993dcde0f612
davidalb97 <davidalb97@hotmail.com>
2021-12-12 19:16:43 +0000

    VED dataset refactoring

---
+++ src/tfm18/ved/__init__.py
@@ -0,0 +1,11 @@
+from src.tfm18.ved import VEDDatasetReader
+from.. import BasicApproach
+
+def main():
+    #VEDDatasetReader.generate_valid_trips()
+    VEDDatasetReader.read_valid_trip()
+    return
+
+
+if __name__ == "__main__":
+    main()
