62607171a16f1a6bc55f9310099b982adff0eda9
davidalb97 <davidalb97@hotmail.com>
2022-01-10 00:05:21 +0000

    Added Emobpy views
    Signed-off-by: davidalb97 <davidalb97@hotmail.com>

---
+++ tfm18/src/main/emobpy/util/ViewStep4GridDemandNBplot.py
@@ -0,0 +1,19 @@
+# This file must be present to allow package existence
+from emobpy import DataBase
+from emobpy.plot import NBplot
+
+from tfm18.src.main.emobpy.util.EmobpyUtil import emobpy_db_location
+
+
+def main():
+    database = DataBase(emobpy_db_location)
+    database.loadfiles_batch(kind="charging")
+    time_series_name = list(database.db.keys())[0]
+    nb_plot = NBplot(database)
+    plot_obj = nb_plot.sgplot_ged(time_series_name)
+    plot_obj.show()
+    return
+
+
+if __name__ == "__main__":
+    main()
