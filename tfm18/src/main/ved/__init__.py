99a385607b6662ea42381743645632ea84aa0176
davidalb97 <davidalb97@hotmail.com>
2022-03-03 10:23:56 +0000

    VED plot example

--- tfm18/src/main/ved/__init__.py
+++ tfm18/src/main/ved/__init__.py
@@ -2,8 +2,9 @@ from tfm18.src.main.ved import VEDDatasetReader
 
 
 def main():
-    VEDDatasetReader.generate_valid_trips()
-    # VEDDatasetReader.read_valid_trip()
+    # VEDDatasetReader.generate_valid_trips()
+    VEDDatasetReader.read_valid_trip('E0/2533_10-AC_ON.csv')
+    VEDDatasetReader.read_valid_trip('E1/772_455-AC_ON.csv')
     return
 
 
