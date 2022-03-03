c232c1a386d736aa088d0b3f0681fe0b2d633177
davidalb97 <davidalb97@hotmail.com>
2022-03-03 08:34:23 +0000

    Fixed VED dataset malformed SOC values

--- tfm18/src/main/ved/VEDInstance.py
+++ tfm18/src/main/ved/VEDInstance.py
@@ -1,3 +1,5 @@
+from infixpy import Seq
+
 csv_splitter = ','
 csv_header = 'DayNum,VehId,Trip,Timestamp(ms),Latitude[deg],Longitude[deg],Vehicle Speed[km/h],MAF[g/sec],' \
              'Engine RPM[RPM],Absolute Load[%],OAT[DegC],Fuel Rate[L/hr],Air Conditioning Power[kW],' \
@@ -55,51 +57,38 @@ class VEDInstance:
         self.long_term_fuel_trim_bank_2: float = instance[21]
 
     def to_csv(self) -> str:
-        ret_str: str = "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s\n" % (
-            self.day_num,
-            csv_splitter,
-            self.veh_id,
-            csv_splitter,
-            self.trip,
-            csv_splitter,
-            self.timestamp_ms,
-            csv_splitter,
-            self.latitude_deg,
-            csv_splitter,
-            self.longitude_deg,
-            csv_splitter,
-            self.vehicle_speed,
-            csv_splitter,
-            self.mass_air_flow,
-            csv_splitter,
-            self.engine_rpm,
-            csv_splitter,
-            self.absolute_load,
-            csv_splitter,
-            self.outside_air_temperature_degrees,
-            csv_splitter,
-            self.fuel_rate,
-            csv_splitter,
-            self.air_conditioning_power_kw,
-            csv_splitter,
-            self.air_conditioning_power_w,
-            csv_splitter,
-            self.heater_power_w,
-            csv_splitter,
-            self.hv_battery_current_amperes,
-            csv_splitter,
-            self.hv_battery_SOC,
-            csv_splitter,
-            self.hv_battery_voltage,
-            csv_splitter,
-            self.short_term_fuel_trim_bank_1,
-            csv_splitter,
-            self.short_term_fuel_trim_bank_2,
-            csv_splitter,
-            self.long_term_fuel_trim_bank_1,
-            csv_splitter,
-            self.long_term_fuel_trim_bank_2
-        )
+        ret_str: str = (
+            Seq(
+                [
+                    self.day_num,
+                    self.veh_id,
+                    self.trip,
+                    self.timestamp_ms,
+                    self.latitude_deg,
+                    self.longitude_deg,
+                    self.vehicle_speed,
+                    self.mass_air_flow,
+                    self.engine_rpm,
+                    self.absolute_load,
+                    self.outside_air_temperature_degrees,
+                    self.fuel_rate,
+                    self.air_conditioning_power_kw,
+                    self.air_conditioning_power_w,
+                    self.heater_power_w,
+                    self.hv_battery_current_amperes,
+                    self.hv_battery_SOC,
+                    self.hv_battery_voltage,
+                    self.short_term_fuel_trim_bank_1,
+                    self.short_term_fuel_trim_bank_2,
+                    self.long_term_fuel_trim_bank_1,
+                    self.long_term_fuel_trim_bank_2
+                ]
+            )
+        ).map(lambda field: str(field) + csv_splitter)\
+            .reduce(lambda x, y: x + y)
 
+        # csv_splitter,
         # Fix Orange replacing NaN with ?
-        return ret_str.replace("?", 'NaN')
+        # Ignore last csv_splitter
+        # Add new line
+        return ret_str.replace("?", 'NaN')[:-1] + "\n"
