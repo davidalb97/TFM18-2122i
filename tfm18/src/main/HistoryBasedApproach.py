63243e1d8fbe9f55ac9486ef5a99aee621973a85
davidalb97 <davidalb97@hotmail.com>
2022-04-11 14:40:54 +0100

    VED dataset reader with HistoryBasedApproach

---
+++ tfm18/src/main/HistoryBasedApproach.py
@@ -0,0 +1,108 @@
+import statistics
+
+import math
+from infixpy import Seq
+
+from tfm18.src.main.util.Formulas import unsafe_mean
+
+
+class HistoryBasedApproach:
+    k: float = 0
+    iecs: list[float] = list()
+    aec_mas: list[float] = list()  # CONFIRMAR!
+    aec: float  # CONFIRMAR!
+    full_battery_energy_FBE: float
+    N: int
+    delta: float
+    min_timestamp_step_ms: int
+    min_instance_energy: int
+    next_timestamp_ms: int = None
+    timestep_iecs: list[float] = list()
+    previous_eRange = 0
+
+    def __init__(self,
+                 N: int,
+                 delta: float,
+                 min_timestamp_step_ms: int,
+                 min_instance_energy: int,
+                 full_battery_energy_FBE: float,
+                 average_energy_consumption_aec: float
+                 ):
+        self.N = N
+        self.delta = delta
+        self.min_timestamp_step_ms = min_timestamp_step_ms
+        self.min_instance_energy = min_instance_energy
+        self.full_battery_energy_FBE = full_battery_energy_FBE
+        self.aec = average_energy_consumption_aec
+
+    def eRange(self, state_of_charge: float, iec, timestamp_ms):
+
+        self.timestep_iecs.append(iec)
+
+        # wait 1 minute
+        if not self.is_min_timestep(timestamp_ms):
+            return self.previous_eRange
+
+        self.k += 1
+
+        # compute moving average discarding zeros due to no consumption
+        aec_lastminute: float = self.average_discardzeros(self.timestep_iecs)  # CONFIRMAR!
+        self.timestep_iecs.clear()
+        self.iecs.append(aec_lastminute)  # CONFIRMAR!
+
+        # Check first if the vehicle is stopped or moving too slow
+        if iec == 0 or aec_lastminute <= self.min_instance_energy:
+            eRange = self.previous_eRange
+        else:
+            last_N_iecs = self.iecs[-self.N:]
+            aec_ma: float = self.average_discardzeros(last_N_iecs)
+            self.aec_mas.append(aec_ma)  # CONFIRMAR!
+
+            # Weighted moving average computation
+            aec_wma: float = self.average_waighted(self.aec_mas)
+
+            # Make step decision
+            if aec_wma < self.aec:
+                self.aec -= self.delta
+            else:
+                self.aec += self.delta
+
+            eRange = math.floor(self.full_battery_energy_FBE / self.aec * state_of_charge)
+
+        if len(self.aec_mas) > self.N:
+            self.aec_mas.pop(0)
+
+        self.previous_eRange = eRange
+
+        return eRange
+
+    def is_min_timestep(self, timestamp_ms) -> bool:
+        if self.next_timestamp_ms is None:
+            self.next_timestamp_ms = self.min_timestamp_step_ms
+            return True
+        elif timestamp_ms > self.next_timestamp_ms:
+            self.next_timestamp_ms += self.min_timestamp_step_ms
+            return True
+        else:
+            return False
+
+    def average_discardzeros(self, list: list):
+        zero_free_list = Seq(list).filter(lambda x: x != 0).tolist()
+        return unsafe_mean(zero_free_list)
+
+    def average_waighted(self, list: list):
+        weighted_list = (
+            Seq(list)
+                .enumerate()
+                # V1/2, V2/4, V3/8, ... VN/2^N
+                .map(lambda idx_value: idx_value[1] / math.pow(2, idx_value[0]))
+                .tolist()
+        )
+        return statistics.mean(weighted_list)
+
+
+def test(FBE: float):
+    FBE = 125  # Km
+    # AEC 176Wh/Km ou 126 Wh/km?
+    AEC = 176
+    k = 0
--- tfm18/src/main/util/Formulas.py
+++ tfm18/src/main/util/Formulas.py
@@ -1,3 +1,5 @@
+import math
+import statistics
 
 
 def calculate_wattage(voltage_V: float, current_A: float) -> float:
@@ -9,7 +11,7 @@ def calculate_wattage(voltage_V: float, current_A: float) -> float:
     return voltage_V * current_A
 
 
-def milliseconds_to_minutes(milies: float) -> float:
+def convert_milliseconds_to_minutes(milies: float) -> float:
     """
     :param milies: The miliseconds to convert.
     :return: The minutes.
@@ -17,7 +19,15 @@ def milliseconds_to_minutes(milies: float) -> float:
     return milies / 1000 / 60
 
 
-def watts_to_kilowatts(watts: float) -> float:
+def convert_milliseconds_to_hours(milies: float) -> float:
+    """
+    :param milies: The miliseconds to convert.
+    :return: The minutes.
+    """
+    return milies / 1000 / 60 / 60
+
+
+def convert_watts_to_kilowatts(watts: float) -> float:
     """
     :param watts: The wattage to convert.
     :return: The kilowatts.
@@ -25,9 +35,70 @@ def watts_to_kilowatts(watts: float) -> float:
     return watts / 1000
 
 
-def kilowatts_to_watts(kilowatts: float) -> float:
+def convert_kilowatts_to_watts(kilowatts: float) -> float:
     """
     :param kilowatts: The kilowattage to convert.
     :return: The kilowatts.
     """
     return kilowatts * 1000
+
+
+def calculate_kilowatts_hour(kilowatts: float, hours: float) -> float:
+    """
+   :param kilowatts: The kilowattage [Kw] to convert.
+   :param hours: The hours [hours] to convert.
+   :return: The kilowatts/hour [Kw/h].
+   """
+    return kilowatts / hours
+
+
+def calculate_aceleration_km_h2(speed_km_h1: float, speed_km_h2: float) -> float:
+    """
+    :param speed_km_h1: Initial speed [km/s].
+    :param speed_km_h2: Final speed [km/s].
+    :return: The aceleration [km/s^2]
+    """
+    return speed_km_h2 - speed_km_h1
+
+
+def calculate_non_linear_distance_km(initial_velocity_km_h: float, aceleration_km_h_: float, time_h: float) -> float:
+    """
+    Calculates the traveled distance of a nonlinear velocity.
+    :param initial_velocity_km_h: The initial speed [km/h].
+    :param aceleration_km_h_: The aceleration [km/h^2].
+    :param time_h: The time travelled [hours].
+    :return: The travel distance [km].
+    """
+    return initial_velocity_km_h * time_h + 1 / 2 * aceleration_km_h_ * math.pow(time_h, 2)
+
+
+def calculate_linear_distance_km(speed_km_h: float, time_h: float) -> float:
+    """
+    Calculates the traveled distance of a nonlinear velocity.
+    :param speed_km_h: The speed [Km/h].
+    :param time_h: The time travelled [hours].
+    :return: The travel distance [Km].
+    """
+    return speed_km_h * time_h
+
+
+def calculate_kwh_100km(kilowats_hour_kwh: float, distance_km: float) -> float:
+    """
+    :param kilowats_hour_kwh: The kilowatts per hour [Kw/h].
+    :param distance_km: The distance traveled [Km].
+    :return: The kilowatts per hour per 100 km [m/s].
+    """
+    if distance_km == 0:
+        return 0
+    # kilowats_hour_kwh - distance_km
+    #       x           - 100 Km
+    return unsafe_division(n=kilowats_hour_kwh * 100, d=distance_km) # CONFIRMAR!
+
+
+# Talvêz retornar negativo?
+def unsafe_division(n, d):
+    return n / d if d else 0
+
+
+def unsafe_mean(list):
+    return statistics.mean(list) if len(list) != 0 else 0
