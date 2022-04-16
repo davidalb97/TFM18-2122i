9bd7ca2ee63dbfe5f437fd994928bec927ca8a28
lamarr <davidalb97@hotmail.com>
2022-04-16 17:23:31 +0100

    Moved VED src from /src/main/ved to /src/main/data/ved

--- tfm18/src/main/data/ved/__init__.py
+++ tfm18/src/main/data/ved/__init__.py
@@ -1,4 +1,4 @@
-from tfm18.src.main.ved import VEDDatasetReader
+from tfm18.src.main.data.ved import VEDDatasetReader
 
 
 def main():
@@ -6,31 +6,3 @@ def main():
     # VEDDatasetReader.read_valid_trip('E0/VED_180530_week_2533_10-AC_ON.csv')
     VEDDatasetReader.read_valid_trip('E1/VED_171213_week_772_455-AC_ON.csv')
     return
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    main()
