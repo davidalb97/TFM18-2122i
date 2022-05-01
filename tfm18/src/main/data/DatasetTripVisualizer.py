feb6d7f6738b7c42f994d543dcfa1ab740dd5f41
davidalb97 <davidalb97@hotmail.com>
2022-05-02 00:29:56 +0100

    DatasetTripVisualizer: Removed comments from old code

--- tfm18/src/main/data/DatasetTripVisualizer.py
+++ tfm18/src/main/data/DatasetTripVisualizer.py
@@ -72,53 +72,10 @@ def plot_dataset_eRange_results(dataset_data: DatasetData):
     mean_quadratic_error = quadratic_error_sum / eRange_entry_count
     print("Mean quadratic error: %s" % mean_quadratic_error)
 
-    # Make plots nonblocking
-    # matplotlib.interactive(True)
-
-    # plt.plot(timestamps, socs)
-    # plt.xlabel('timestamps (ms)')
-    # plt.ylabel('SOC (%))')
-    # plt.show()
-    # print()
-    #
-    # plt.plot(timestamps, kilowatts)
-    # plt.xlabel('timestamps (ms)')
-    # plt.ylabel('kilowatts (w))')
-    # plt.show()
-    # print()
-
-    # plot1: Figure = plt.figure(1)
-    # plt.plot(timestamps, socs)
-    # plot2: Figure = plt.figure(2)
-    # plt.plot(timestamps, kilowatts)
-    # plt.show()
-    fig_offset = 2
-    # gs = gridspec.GridSpec(3 * fig_offset, 2 * fig_offset)
-
-    # plt.subplot(1, 2, 1) # row 1, col 2 index 1
-    # fig, axs = pyplot.subplots(4, 2)  # Create the figure and axes object
-    # SOC_axis = axs[0, 0]
-    # iec_axis = axs[0, 1]
-    # eRange_axis = axs[1, 0]
-    # current_axis = axs[1, 1]
-    # power_axis = axs[2, 0]
-    # speed_axis = axs[2, 1]
-    # aec_axis = axs[3, 1]
-
-    # fig.tight_layout()  # otherwise the right y-label is slightly clipped
-
     # marker = "o"
     marker = None
     # fontsize = 12
     fontsize = None
-    # figure = pyplot.figure(figsize=(6 * fig_offset, 3 * fig_offset))
-    # SOC_axis = figure.add_subplot(gs[:2, :2])
-    # eRange_axis = figure.add_subplot(gs[:2, 2:])
-    # power_axis = figure.add_subplot(gs[:4, :2])
-    # iec_axis = figure.add_subplot(gs[:4, 2:])
-    # current_axis = figure.add_subplot(gs[:6, :2])
-    # speed_axis = figure.add_subplot(gs[:6, 2:])
-    # aec_axis = figure.add_subplot(gs[4:6, 1:3])
 
     fig: Figure
     axs: dict[str, Axes]
@@ -144,7 +101,6 @@ def plot_dataset_eRange_results(dataset_data: DatasetData):
         ],
         constrained_layout=True
     )
-    items: list[tuple[str, Axes]] = axs.items()
 
     SOC_axis = axs[soc_key]
     iec_axis = axs[iec_key]
@@ -154,20 +110,7 @@ def plot_dataset_eRange_results(dataset_data: DatasetData):
     speed_axis = axs[speed_key]
     aec_axis = axs[aec_key]
 
-    timestamps_min_point = min(timestamps_min)
-    timestamps_max_point = max(timestamps_min)
-
     # SOC Graph
-    # color = 'blue'
-    # SOC_axis.plot(timestamps_min, socs, color=color, marker=marker)
-    # SOC_axis.set_xlabel('time [min]', fontsize=fontsize)
-    # SOC_axis.set_ylabel('SOC (%)', color=color, fontsize=fontsize)
-    # SOC_axis.tick_params(axis='y', labelcolor=color)
-    # socs_min_point = min(socs)
-    # socs_max_point = max(socs)
-    # SOC_axis.set_ylim(socs_min_point, socs_max_point)
-    # SOC_axis.set_xlim(timestamps_min_point, timestamps_max_point)
-    # pyplot.gca().yaxis.set_major_locator(pyplot.MultipleLocator(5))
     configure_plot(
         axis=SOC_axis,
         x_axis_points=timestamps_min,
@@ -180,7 +123,6 @@ def plot_dataset_eRange_results(dataset_data: DatasetData):
     )
 
     # Power Graph
-    # eRange axis
     power_axis_list = configure_plot(
         axis=power_axis,
         x_axis_points=timestamps_min,
@@ -191,36 +133,6 @@ def plot_dataset_eRange_results(dataset_data: DatasetData):
         fontsize=None,
         marker=None
     )
-    # power_all_points = kilowatts.copy()
-    # power_all_points.extend(ac_kilowatts)
-    # power_min_point = min(power_all_points)
-    # power_max_point = max(power_all_points)
-    #
-    # color = 'red'
-    # power_axis.plot(timestamps_min, kilowatts, color=color, marker=marker)
-    # power_axis.set_ylabel("Battery power [Kw]", color=color, fontsize=fontsize)
-    # power_axis.tick_params(axis='y', labelcolor=color)
-    # power_axis.set_ylim(power_min_point, power_max_point)
-    #
-    # color = 'green'
-    # timestamps_ac_kilowatts = power_axis.twinx()
-    # timestamps_ac_kilowatts.plot(timestamps_min, ac_kilowatts, color=color, marker=marker)
-    # timestamps_ac_kilowatts.set_ylabel("AC power [Kw]", color=color, fontsize=fontsize)
-    # timestamps_ac_kilowatts.tick_params(axis='y', labelcolor=color)
-    # # power_axis.get_shared_y_axes() \
-    # #     .join(timestamps_kilowatts, timestamps_ac_kilowatts)
-    # timestamps_ac_kilowatts.set_ylim(power_min_point, power_max_point)
-    # timestamps_ac_kilowatts.set_ylim(power_min_point, power_max_point)
-    #
-    # power_axis.sharey(timestamps_ac_kilowatts)
-    # pyplot.gca().yaxis.set_major_locator(pyplot.MultipleLocator(5))
-
-    # plt.show(block=True)
-    #
-    # plt.plot(timestamps, basic_erange)
-    # plt.xlabel('time [min]')
-    # plt.ylabel('eRange [Km])')
-    # plt.show(block=True)
 
     # eRange axis
     eRange_axis_list = configure_plot(
@@ -233,26 +145,6 @@ def plot_dataset_eRange_results(dataset_data: DatasetData):
         fontsize=None,
         marker=None
     )
-    # color = 'blue'
-    # eRange_axis.plot(timestamps_min, eRange_basic_list, color=color, marker=marker)
-    # eRange_axis.set_xlabel('time [min]', fontsize=fontsize)
-    # eRange_axis.set_ylabel('basic eRange [Km]', color=color, fontsize=fontsize)
-    # eRange_axis.tick_params(axis='y', labelcolor=color)
-    #
-    # color = 'red'
-    # eRange_history_plot = eRange_axis.twinx()
-    # eRange_history_plot.plot(timestamps_min, eRange_history_list, color=color, marker=marker)
-    # eRange_history_plot.set_ylabel("history based eRange [Km]", color=color, fontsize=fontsize)
-    # eRange_history_plot.tick_params(axis='y', labelcolor=color)
-    # eRange_axis.sharey(eRange_history_plot)
-    #
-    # eRange_all_points = eRange_basic_list.copy()
-    # eRange_all_points.extend(eRange_history_list)
-    # eRange_minPoint = min(eRange_all_points)
-    # eRange_maxPoint = max(eRange_all_points)
-    # eRange_axis.set_ylim(eRange_minPoint, eRange_maxPoint)
-    # eRange_history_plot.set_ylim(eRange_minPoint, eRange_maxPoint)
-    # pyplot.gca().yaxis.set_major_locator(pyplot.MultipleLocator(5))
 
     # Current axis
     iec_axis_list = configure_plot(
@@ -265,11 +157,6 @@ def plot_dataset_eRange_results(dataset_data: DatasetData):
         fontsize=None,
         marker=None
     )
-    # color = 'blue'
-    # iec_axis.plot(timestamps_min, iecs, color=color, marker=marker)
-    # iec_axis.set_xlabel('time [min]', fontsize=fontsize)
-    # iec_axis.set_ylabel('Energy [KWh/100km]', color=color, fontsize=fontsize)
-    # iec_axis.tick_params(axis='y', labelcolor=color)
 
     # Current axis
     current_axis_list = configure_plot(
@@ -282,11 +169,6 @@ def plot_dataset_eRange_results(dataset_data: DatasetData):
         fontsize=None,
         marker=None
     )
-    # color = 'blue'
-    # current_axis.plot(timestamps_min, currents, color=color, marker=marker)
-    # current_axis.set_xlabel('time [min]', fontsize=fontsize)
-    # current_axis.set_ylabel('Current [A]', color=color, fontsize=fontsize)
-    # current_axis.tick_params(axis='y', labelcolor=color)
 
     # Speed axis
     speed_axis_list = configure_plot(
@@ -299,11 +181,6 @@ def plot_dataset_eRange_results(dataset_data: DatasetData):
         fontsize=None,
         marker=None
     )
-    # color = 'blue'
-    # speed_axis.plot(timestamps_min, speeds, color=color, marker=marker)
-    # speed_axis.set_xlabel('time [min]', fontsize=fontsize)
-    # speed_axis.set_ylabel('Speed [Km/h]', color=color, fontsize=fontsize)
-    # speed_axis.tick_params(axis='y', labelcolor=color)
 
     # AECs
     aec_axis_list = configure_plot(
@@ -317,33 +194,6 @@ def plot_dataset_eRange_results(dataset_data: DatasetData):
         marker=None
     )
     aec_axis_list[2].spines["right"].set_position(("axes", 1.1))
-
-    # color = 'purple'
-    # aec_axis.plot(historyBasedApproach.times_acc, historyBasedApproach.aecs_acc, color=color, marker=marker)
-    # aec_axis.set_ylabel('aec [kWh/100Km]', color=color, fontsize=fontsize)
-    # aec_axis.set_xlabel('time [min]', fontsize=fontsize)
-    # aec_axis.tick_params(axis='y', labelcolor=color)
-    #
-    # color = 'goldenrod'
-    # aec_wma_axis = aec_axis.twinx()
-    # aec_wma_axis.plot(historyBasedApproach.times_acc, historyBasedApproach.aecs_wma_acc, color=color, marker=marker)
-    # aec_wma_axis.set_ylabel('aec_wma [kWh/100Km]', color=color, fontsize=fontsize)
-    # aec_wma_axis.tick_params(axis='y', labelcolor=color)
-    # aec_axis.sharey(aec_wma_axis)
-    #
-    # color = 'chocolate'
-    # aec_ma_axis = aec_axis.twinx()
-    # aec_ma_axis.plot(historyBasedApproach.times_acc, historyBasedApproach.aecs_ma_acc, color=color, marker=marker)
-    # aec_ma_axis.set_ylabel('aec_ma [kWh/100Km]', color=color, fontsize=fontsize)
-    # aec_ma_axis.tick_params(axis='y', labelcolor=color)
-    # aec_wma_axis.sharey(aec_ma_axis)
-
-    #aec_wma_axis.spines["right"].set_position(("axes", 1.1))
-    # fig.subplots_adjust(right=1.50)
-    # fig.tight_layout()  # otherwise the right y-label is slightly clipped
-
-    # Hide unused axis
-    # fig.delaxes(axs[3][0])
 
     pyplot.show(block=True)
 
