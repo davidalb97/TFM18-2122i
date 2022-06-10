754abd4e687f17b6a8bb78f66ee53f5e584b4595
davidalb97 <davidalb97@hotmail.com>
2022-06-10 22:47:18 +0100

    MyEnsemble now using StratifiedKFold for final estimator

--- tfm18/src/main/algorithm/MyEnsemble.py
+++ tfm18/src/main/algorithm/MyEnsemble.py
@@ -2,6 +2,7 @@ from sklearn import linear_model
 from pandas import DataFrame
 from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
 from sklearn.ensemble import StackingRegressor
+from sklearn.model_selection import StratifiedKFold
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 
@@ -9,13 +10,13 @@ from tfm18.src.main.algorithm.MyRegressor import MyRegressor
 
 
 class MyEnsemble(MyRegressor):
-    estimators = [
-        ('dtr', DecisionTreeRegressor(random_state=0)),
-        ('rfr', RandomForestRegressor(max_depth=2, random_state=0)),
-        ('knr', KNeighborsRegressor(n_neighbors=10, metric='euclidean'))
-    ]
-    regressor = StackingRegressor(
-        estimators=estimators
+    regressor: StackingRegressor = StackingRegressor(
+        estimators=[
+            ('dtr', DecisionTreeRegressor(random_state=0)),
+            ('rfr', RandomForestRegressor(max_depth=2, random_state=0)),
+            ('knr', KNeighborsRegressor(n_neighbors=10, metric='euclidean'))
+        ],
+        final_estimator=StratifiedKFold(n_splits=10, random_state=None, shuffle=False)
     )
 
     def learn(self, input_dataframe: DataFrame, output_dataframe: DataFrame):
